db9f423b03a72000f99a8387e365e02bcf769d98
MaksGrishaev <gm.grishaev@gmail.com>
2018-02-05 16:33:16 +0200

    hw9 task3 - circle

--- hw9/task3/CircleDemo.java
+++ hw9/task3/CircleDemo.java
@@ -2,11 +2,19 @@ package hw9.task3;
 
 public class CircleDemo {
     public static void main(String[] args) {
-        Circle circle = new Circle(5.5);
-        System.out.println(circle);
+        Circle circle1 = new Circle(5.5);
+        Circle circle2 = new Circle(10);
+
+        System.out.println(circle1);
         System.out.printf("Площадь круга радиусом %.2f: %.2f\n",
-                circle.getRadiuse(), circle.squareCircle());
+                circle1.getRadiuse(), circle1.squareCircle());
         System.out.printf("Длина окружности радиусом %.2f: %.2f\n",
-                circle.getRadiuse(), circle.lengthCircle());
+                circle1.getRadiuse(), circle1.lengthCircle());
+
+        System.out.println(circle2);
+        System.out.printf("Площадь круга радиусом %.2f: %.2f\n",
+                circle2.getRadiuse(), circle2.squareCircle());
+        System.out.printf("Длина окружности радиусом %.2f: %.2f\n",
+                circle2.getRadiuse(), circle2.lengthCircle());
     }
 }
