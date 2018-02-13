d153c2937e532ccabe93b9d02b7fd37162ab611a
MaksGrishaev <gm.grishaev@gmail.com>
2018-02-13 14:18:45 +0200

    rekursiya if (a > b) { if (a == b) { - это условие всегда будет false. return Integer.toString(a); }
    исправил a >= b

--- hw6/task5/Rekursiya.java
+++ hw6/task5/Rekursiya.java
@@ -4,10 +4,11 @@ public class Rekursiya {
     public static void main(String[] args) {
         System.out.println(intFromA2B(4, 10));
         System.out.println(intFromA2B(4, -10));
+        System.out.println(intFromA2B(4, 4));
     }
 
     public static String intFromA2B(int a, int b) {
-        if (a > b) {
+        if (a >= b) {
             if (a == b) {
                 return Integer.toString(a);
             }
