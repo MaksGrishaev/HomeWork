044f6e2e86eb533649e4480c1002456ce05d4f09
MaksGrishaev <gm.grishaev@gmail.com>
2018-02-13 14:13:43 +0200

    printable 1)Желательно переопределять в создаваемом классе методы toString, equals и hashCode.

--- hw10/task1/Book.java
+++ hw10/task1/Book.java
@@ -23,4 +23,19 @@ public class Book implements Printable {
     public void print() {
         System.out.printf("\n%s \n\tTitle:%s", getType(), getTitle());
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
--- hw10/task1/Magazine.java
+++ hw10/task1/Magazine.java
@@ -32,4 +32,19 @@ public class Magazine implements Printable {
             }
         }
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
